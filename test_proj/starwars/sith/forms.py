3413ff74bc140c8194d8c4cbc5c09c00a50a0424
Dmitry099 <54704017+Dmitry099@users.noreply.github.com>
2019-11-14 08:59:21 +0300

    Correction recruiter form

--- test_proj/starwars/sith/forms.py
+++ test_proj/starwars/sith/forms.py
@@ -7,7 +7,7 @@ class RecruterForm(forms.ModelForm):
 
     class Meta:
         model = Recruter
-        exclude = ('is_shadow_hand', 'sith')
+        exclude = ('sith')
 
 
 class AnswerForm(forms.ModelForm):
